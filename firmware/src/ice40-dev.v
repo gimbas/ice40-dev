be30962ad792d939b6764818a8171060bcb928b2
João Silva <jgc3silva@gmail.com>
2019-09-11 09:38:16 +0100

    Change PCF model

--- firmware/src/ice40-dev.v
+++ firmware/src/ice40-dev.v
@@ -1,7 +1,7 @@
-module top(input CLK_25MHZ, output LED);
+module top(input clk, output LED);
 
 reg [23:0] counter = 0;
 assign LED = counter[23];
-always @(posedge CLK_25MHZ) counter <= counter + 1;
+always @(posedge clk) counter <= counter + 1;
 
 endmodule
